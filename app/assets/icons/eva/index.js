c6128313158d373c0983562753a687f85ae1d786
Eddie Padin <eddie@entertainermedia.net>
2019-10-07 12:02:19 -0500

    add themeprovider; - add eva-design theme configs & eva icons;

---
+++ app/assets/icons/eva/index.js
@@ -0,0 +1,725 @@
+import { IconSource, EvaIcon } from './icon';
+
+export const MenuIconAuth = style => {
+  const source = {
+    imageSource: require('./icon-auth.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconAuthDark = style => {
+  const source = {
+    imageSource: require('./icon-auth-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconSocial = style => {
+  const source = {
+    imageSource: require('./icon-social.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconSocialDark = style => {
+  const source = {
+    imageSource: require('./icon-social-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconArticles = style => {
+  const source = {
+    imageSource: require('./icon-articles.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconArticlesDark = style => {
+  const source = {
+    imageSource: require('./icon-articles-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconMessaging = style => {
+  const source = {
+    imageSource: require('./icon-messaging.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconMessagingDark = style => {
+  const source = {
+    imageSource: require('./icon-messaging-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconDashboards = style => {
+  const source = {
+    imageSource: require('./icon-dashboards.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconDashboardsDark = style => {
+  const source = {
+    imageSource: require('./icon-dashboards-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconEcommerce = style => {
+  const source = {
+    imageSource: require('./icon-ecommerce.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MenuIconEcommerceDark = style => {
+  const source = {
+    imageSource: require('./icon-ecommerce-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconAvatar = style => {
+  const source = {
+    imageSource: require('./icon-avatar.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconAvatarDark = style => {
+  const source = {
+    imageSource: require('./icon-avatar-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconButton = style => {
+  const source = {
+    imageSource: require('./icon-button.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconButtonDark = style => {
+  const source = {
+    imageSource: require('./icon-button-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconButtonGroup = style => {
+  const source = {
+    imageSource: require('./icon-button-group.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconButtonGroupDark = style => {
+  const source = {
+    imageSource: require('./icon-button-group-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconCheckBox = style => {
+  const source = {
+    imageSource: require('./icon-checkbox.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconCheckBoxDark = style => {
+  const source = {
+    imageSource: require('./icon-checkbox-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconInput = style => {
+  const source = {
+    imageSource: require('./icon-input.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconInputDark = style => {
+  const source = {
+    imageSource: require('./icon-input-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconList = style => {
+  const source = {
+    imageSource: require('./icon-list.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconListDark = style => {
+  const source = {
+    imageSource: require('./icon-list-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconOverflowMenu = style => {
+  const source = {
+    imageSource: require('./icon-overflow-menu.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconOverflowMenuDark = style => {
+  const source = {
+    imageSource: require('./icon-overflow-menu-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconPopover = style => {
+  const source = {
+    imageSource: require('./icon-popover.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconPopoverDark = style => {
+  const source = {
+    imageSource: require('./icon-popover-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconRadio = style => {
+  const source = {
+    imageSource: require('./icon-radio.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconRadioDark = style => {
+  const source = {
+    imageSource: require('./icon-radio-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconTabView = style => {
+  const source = {
+    imageSource: require('./icon-tab-view.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconTabViewDark = style => {
+  const source = {
+    imageSource: require('./icon-tab-view-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconText = style => {
+  const source = {
+    imageSource: require('./icon-text.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconTextDark = style => {
+  const source = {
+    imageSource: require('./icon-text-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconToggle = style => {
+  const source = {
+    imageSource: require('./icon-toggle.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconToggleDark = style => {
+  const source = {
+    imageSource: require('./icon-toggle-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconTooltip = style => {
+  const source = {
+    imageSource: require('./icon-tooltip.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconTooltipDark = style => {
+  const source = {
+    imageSource: require('./icon-tooltip-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconTopNavigation = style => {
+  const source = {
+    imageSource: require('./icon-top-navigation.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconTopNavigationDark = style => {
+  const source = {
+    imageSource: require('./icon-top-navigation-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconBottomNavigation = style => {
+  const source = {
+    imageSource: require('./icon-bottom-navigation.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconBottomNavigationDark = style => {
+  const source = {
+    imageSource: require('./icon-bottom-navigation-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconModal = style => {
+  const source = {
+    imageSource: require('./icon-modal.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ComponentsIconModalDark = style => {
+  const source = {
+    imageSource: require('./icon-modal-dark.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ArrowHeadDownIconFill = style => {
+  const source = {
+    imageSource: require('./arrowhead-down.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ArrowHeadUpIconFill = style => {
+  const source = {
+    imageSource: require('./arrowhead-up.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ArrowIosBackFill = style => {
+  const source = {
+    imageSource: require('./arrow-ios-back.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const BulbIconFill = style => {
+  const source = {
+    imageSource: require('./bulb.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const CameraIconFill = style => {
+  const source = {
+    imageSource: require('./camera.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const CartIconOutline = style => {
+  const source = {
+    imageSource: require('./shopping-cart-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const CartIconFill = style => {
+  const source = {
+    imageSource: require('./shopping-cart.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ClockIconOutline = style => {
+  const source = {
+    imageSource: require('./clock-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const EmailIconFill = style => {
+  const source = {
+    imageSource: require('./email.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const EyeOffIconFill = style => {
+  const source = {
+    imageSource: require('./eye-off.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ColorPaletteIconOutline = style => {
+  const source = {
+    imageSource: require('./color-palette-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ArrowForwardIconOutline = style => {
+  const source = {
+    imageSource: require('./arrow-forward-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const GridIconOutline = style => {
+  const source = {
+    imageSource: require('./grid-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const HeartIconFill = style => {
+  const source = {
+    imageSource: require('./heart.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const LayoutIconOutline = style => {
+  const source = {
+    imageSource: require('./layout-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const LockIconFill = style => {
+  const source = {
+    imageSource: require('./lock.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MessageCircleIconFill = style => {
+  const source = {
+    imageSource: require('./message-circle.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MessageCircleIconOutline = style => {
+  const source = {
+    imageSource: require('./message-circle-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MoreHorizontalIconFill = style => {
+  const source = {
+    imageSource: require('./more-horizontal.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const PersonIconFill = style => {
+  const source = {
+    imageSource: require('./person.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const PhoneIconFill = style => {
+  const source = {
+    imageSource: require('./phone.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const PersonAddIconFill = style => {
+  const source = {
+    imageSource: require('./person-add.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const PinIconFill = style => {
+  const source = {
+    imageSource: require('./pin.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const PinIconOutline = style => {
+  const source = {
+    imageSource: require('./pin-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const PlusIconFill = style => {
+  const source = {
+    imageSource: require('./plus.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MinusIconFill = style => {
+  const source = {
+    imageSource: require('./minus.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const SearchIconOutline = style => {
+  const source = {
+    imageSource: require('./search-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const StarIconFill = style => {
+  const source = {
+    imageSource: require('./star.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const StarIconOutline = style => {
+  const source = {
+    imageSource: require('./star-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const FlashIconOutline = style => {
+  const source = {
+    imageSource: require('./flash-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const DoneAllIconOutline = style => {
+  const source = {
+    imageSource: require('./done-all-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MoreVerticalIconFill = style => {
+  const source = {
+    imageSource: require('./more-vertical.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const CreditCardIconFill = style => {
+  const source = {
+    imageSource: require('./credit-card.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const CloseIconOutline = style => {
+  const source = {
+    imageSource: require('./close-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MicIconFill = style => {
+  const source = {
+    imageSource: require('./mic.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const PaperPlaneIconFill = style => {
+  const source = {
+    imageSource: require('./paper-plane.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ImageIconFill = style => {
+  const source = {
+    imageSource: require('./image.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const FileTextIconFill = style => {
+  const source = {
+    imageSource: require('./file-text.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const MapIconFill = style => {
+  const source = {
+    imageSource: require('./map.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const PeopleIconFill = style => {
+  const source = {
+    imageSource: require('./people.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const PlayCircleIconFill = style => {
+  const source = {
+    imageSource: require('./play-circle.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ShareIconOutline = style => {
+  const source = {
+    imageSource: require('./share-outline.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const ListIconFill = style => {
+  const source = {
+    imageSource: require('./list.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const GoogleIconFill = style => {
+  const source = {
+    imageSource: require('./google.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const FacebookIconFill = style => {
+  const source = {
+    imageSource: require('./facebook.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const TwitterIconFill = style => {
+  const source = {
+    imageSource: require('./twitter.png'),
+  };
+
+  return IconSource(source, style);
+};
+
+export const EyeOutline = style => {
+  return EvaIcon({ name: 'eye-outline', style });
+};
