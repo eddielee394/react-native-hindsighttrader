cde8b4096c4d6892d4d7037657131a65e985c5e6
Eddie Padin <eddie@entertainermedia.net>
2019-10-08 03:24:22 -0500

    add navigation functionality (WIP); - refactor root navigation for better maintainability; - remove ui-kitten bottom nav; - integrate rnrf dynamic tabbed navigation;

--- app/components/Navigation/BottomNavigationContainer.js
+++ app/components/Navigation/BottomNavigationContainer.js
@@ -1,32 +1,88 @@
 import React from 'react';
 import {
-  EyeOutline,
-  MenuIconDashboards,
-  SearchIconOutline,
-} from '../../assets/icons';
-import { BottomNavigation, BottomNavigationTab } from 'react-native-ui-kitten';
+  Footer,
+  ScrollableTab,
+  Tab,
+  TabHeading,
+  Tabs,
+  Text,
+  Icon,
+  Container,
+  Button,
+} from 'native-base';
+import { StyleSheet } from 'react-native';
+import theme from '../../theme';
 
 const BottomNavigationContainer = props => {
-  const { index } = props.navigation.state;
+  const { index, routes } = props.navigation.state;
 
   const onTabSelect = index => {
-    const { [index]: selectedRoute } = props.navigation.state.routes;
+    const { [index]: selectedRoute } = routes;
 
     props.navigation.navigate({
       routeName: selectedRoute.routeName,
     });
   };
 
+  const renderTabHeading = (title, icon, iconType) => (
+    <TabHeading style={styles.navTabHeading}>
+      <Icon
+        name={icon}
+        type={iconType ?? 'Ionicons'}
+        style={styles.navTabIcon}
+      />
+      <Text style={styles.navTabHeadingTitle}>{title}</Text>
+    </TabHeading>
+  );
+
   return (
-    <BottomNavigation
-      appearance="noIndicator"
-      selectedIndex={index}
-      onSelect={onTabSelect}>
-      <BottomNavigationTab title="Watchlists" icon={EyeOutline} />
-      <BottomNavigationTab title="Search" icon={SearchIconOutline} />
-      <BottomNavigationTab title="News" icon={MenuIconDashboards} />
-    </BottomNavigation>
+    <Footer>
+      <Tabs
+        renderTabBar={() => <ScrollableTab style={styles.navTabBar} />}
+        initialPage={index}
+        tabBarUnderlineStyle={styles.navTabUnderlineStyle}
+        tabBarPosition="bottom"
+        onChangeTab={tab => onTabSelect(tab.i)}
+        style={styles.navTabBar}
+        tabBgColor={styles.navTabBar.backgroundColor}>
+        {routes.map(route => {
+          return (
+            <Tab
+              key={route.key}
+              heading={renderTabHeading(
+                route.routes[0].params.title,
+                route.routes[0].params.icon,
+                route.routes[0].params.iconType,
+              )}
+              tabStyle={styles.navTabContent}
+            />
+          );
+        })}
+      </Tabs>
+    </Footer>
   );
 };
+
+const styles = StyleSheet.create({
+  navTabBar: {
+    backgroundColor: theme.dark.tabBgColor,
+    borderBottomWidth: 0,
+  },
+  navTabUnderlineStyle: {
+    top: 0,
+  },
+  navTabHeading: {
+    flexDirection: 'column',
+  },
+  navTabHeadingTitle: {
+    fontSize: 12,
+  },
+  navTabIcon: {
+    fontSize: 20,
+  },
+  navTabContent: {
+    display: 'none',
+  },
+});
 
 export default BottomNavigationContainer;
