0514d82adfe4f7ddddf35b23df21b5dbb521e45a
Eddie Padin <eddie@entertainermedia.net>
2019-10-08 03:20:05 -0500

    ui updates; - add custom dark theme;

---
+++ app/theme/components/Tab.js
@@ -0,0 +1,10 @@
+// @flow
+import variable from '../variables/dark';
+export default () => {
+  const tabTheme = {
+    flex: 1,
+    backgroundColor: variable.tabBgColor,
+  };
+
+  return tabTheme;
+};
---
+++ app/theme/components/TabHeading.js
@@ -0,0 +1,48 @@
+// @flow
+
+import variable from '../variables/platform';
+import { PLATFORM } from '../variables/commonColor';
+
+export default (variables /* : * */ = variable) => {
+  const platform = variables.platform;
+
+  const tabHeadingTheme = {
+    flexDirection: 'row',
+    backgroundColor: variables.tabDefaultBg,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    '.scrollable': {
+      paddingHorizontal: 20,
+      flex: platform === PLATFORM.ANDROID ? 0 : 1,
+      minWidth: platform === PLATFORM.ANDROID ? undefined : 60,
+    },
+    'NativeBase.Footer': {
+      'NativeBase.Icon': {
+        fontSize: variable.tabBarIconFontSize,
+      },
+      'NativeBase.Text': {
+        fontSize: variable.tabBarTextSize,
+      },
+    },
+    'NativeBase.Text': {
+      color: variables.topTabBarTextColor,
+      marginHorizontal: 7,
+    },
+    'NativeBase.Icon': {
+      color: variables.topTabBarTextColor,
+      fontSize: platform === PLATFORM.IOS ? 26 : undefined,
+    },
+    '.active': {
+      'NativeBase.Text': {
+        color: variables.topTabBarActiveTextColor,
+        fontWeight: '600',
+      },
+      'NativeBase.Icon': {
+        color: variables.topTabBarActiveTextColor,
+      },
+    },
+  };
+
+  return tabHeadingTheme;
+};
